429dbe796c9a8bfa267d7ef7afcbfae97e06ae3e
nasMelody <122706874+nasMelody@users.noreply.github.com>
2023-03-16 19:24:13 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,4 @@ const home = require('../controllers/user-controller');
 router.get('/', home.getAllExams);  
       
 
-//const url = "mongodb+srv://techdive12:5boIQEOOBjF2W9xL@covid19imaging.tedajh9.mongodb.net/?retryWrites=true&w=majority"; //Mongodb link
-//const dbName ='Covid19Data';//
-
-///add main
-
-
-
 module.exports = router;
